d0b8b28062d420b5446ff514353a4720a9f0532b
e <stefanycristinas009@gmail.com>
2025-07-28 22:52:15 -0300

    feat: configurar Gson e criar método para converter JSON em objeto Java

---
+++ src/br/com/conversordemoedas/ConverterMoedas.java
@@ -0,0 +1,17 @@
+package br.com.conversordemoedas;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ConverterMoedas {
+    //criando objeto Gson personalizado com GsonBuider
+    Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES) //converter campos snake_case em camelCase
+            .create();
+
+    public void conversao(String json){
+
+    }
+
+}
